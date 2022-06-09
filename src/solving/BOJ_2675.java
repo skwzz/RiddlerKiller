22ff091f30d8e5929010f8cc8fa8d7b431b54d50
KiwoongSon <skw9108@ibizsoftware.net>
2022-06-09 17:01:41 +0900

    2675 풀이중

---
+++ src/solving/BOJ_2675.java
@@ -0,0 +1,24 @@
+package solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 문자열 반복
+ * https://www.acmicpc.net/problem/2675
+ */
+public class BOJ_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int testCase = Integer.parseInt(br.readLine());
+        while(testCase > 0){
+
+            testCase--;
+        }
+
+
+    }
+}
