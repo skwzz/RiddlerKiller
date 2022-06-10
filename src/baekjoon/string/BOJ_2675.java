cddb31ecc96822e5dd5d7d419216c0af0bf28ca3
KiwoongSon <skw9108@ibizsoftware.net>
2022-06-10 10:51:51 +0900

    1157 - 단어공부 2675 - 문자열 반복

--- src/baekjoon/string/BOJ_2675.java
+++ src/baekjoon/string/BOJ_2675.java
@@ -1,8 +1,9 @@
-package solving;
+package baekjoon.string;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * 문자열 반복
@@ -12,13 +13,24 @@ public class BOJ_2675 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int testCase = Integer.parseInt(br.readLine());
         while(testCase > 0){
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            char[] arr = st.nextToken().toCharArray();
 
+            for(int i=0; i<arr.length; i++){
+                for(int j=0; j<n; j++){
+                    sb.append(arr[i]);
+                }
+            }
+
+            sb.append("\n");
             testCase--;
         }
-
+        System.out.print(sb);
 
     }
 }
---
+++ src/solving/BOJ_1157.java
@@ -0,0 +1,45 @@
+package solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 단어 공부
+ * https://www.acmicpc.net/problem/1157
+ */
+public class BOJ_1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] arr = new int[26];
+        char[] str = br.readLine().toCharArray();
+        int maxV = 0;
+        int maxI = 0;
+        int dupCnt = 0;
+
+        for(int i=0; i<str.length; i++){
+            // 97 122 소문자
+            // 65 90 대문자
+            int n = str[i];
+            n = (n>=97) ? n-97 : n-65;
+
+            arr[n]++;
+            if(maxV < arr[n]){
+                maxV = arr[n];
+                maxI = n;
+                dupCnt = 1;
+            }else if(maxV == arr[n]){
+                dupCnt++;
+            }
+        }
+        if(dupCnt == 1){
+            System.out.println((char)(maxI+65));
+        }else{
+            System.out.println("?");
+        }
+    }
+}
