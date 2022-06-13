b8305a81c8ace22bb4fa0aa6bdaf26a3ddb94765
KiwoongSon <skw9108@ibizsoftware.net>
2022-06-13 13:12:37 +0900

    백준 문제풀이 풀이중 2292 - 벌집

---
+++ src/solving/BOJ_2292.java
@@ -0,0 +1,28 @@
+package solving;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 벌집
+ * https://www.acmicpc.net/problem/2292
+ */
+public class BOJ_2292 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int target = Integer.parseInt(br.readLine()); // 1 ~ 10억
+        int sum = 1;
+        int answer = 0;
+
+        while(true){
+            sum = sum + (6*answer);
+            if(target <= sum){
+                break;
+            }
+            answer++;
+        }
+        System.out.println(answer+1);
+    }
+}
