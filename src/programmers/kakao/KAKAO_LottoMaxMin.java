06f680855648d14af78800bca4b548bc47425967
Kiwoong <kidol9108@gmail.com>
2022-06-16 15:41:16 +0900

    문제 정보 추가 2021 카카오 - 로또의 최고 순위와 최저 순위

--- src/programmers/kakao/KAKAO_LottoMaxMin.java
+++ src/programmers/kakao/KAKAO_LottoMaxMin.java
@@ -2,6 +2,10 @@ package programmers.kakao;
 
 import java.util.Arrays;
 
+/**
+ * 로또의 최고 순위와 최저 순위
+ * https://programmers.co.kr/learn/courses/30/lessons/77484
+ */
 public class KAKAO_LottoMaxMin {
     public static void main(String[] args) {
         int[] arr1_1 = new int[]{44, 1, 0, 0, 31, 25};
