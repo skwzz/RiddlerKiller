e57915164f8a80d0717bb37b140296aab4072e9c
Kiwoong <kidol9108@gmail.com>
2022-06-24 15:57:33 +0900

    백준 알고리즘 풀이 2447 - 별찍기-10

--- src/baekjoon/recursion/BOJ_2447.java
+++ src/baekjoon/recursion/BOJ_2447.java
@@ -13,12 +13,15 @@ public class BOJ_2447 {
     static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // init
         int n = Integer.parseInt(br.readLine());
-
         arr = new int[6561][6561];
         arr[0][0] = 1;
+        
+        // recursion
         solution(1, n);
 
+        // print
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
@@ -33,13 +36,13 @@ public class BOJ_2447 {
         System.out.print(sb);
     }
 
-    static void solution(int depth, int dest){ // 2, 9
+    static void solution(int depth, int dest){
         int cur = (int)Math.pow(3, depth);
-        if(cur <= dest){
+        if(cur <= dest){ // recursion exit condition
             int jump = (int)Math.pow(3, depth-1); // 3
             for(int i=0; i<3; i++){
                 for(int j=0; j<3; j++){
-                    if(!(i==2&&j==2) && (i==j)){continue;} //중간에는 스킵
+                    if(!(i==2&&j==2) && (i==j)){continue;} // i와 j가 같은 경우는 우측하단만 실행하도록
 
                     int targetI = 0;
                     int targetJ = 0;
